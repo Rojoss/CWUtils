ada51da63d6b85422f26914ae2ded20a69a96d2f
worstboy32 <clashwars@hotmail.com>
2014-09-22 21:35:19 +0200

    Fixed tips

--- CWUtils/src/main/java/com/clashwars/cwutils/config/TipConfig.java
+++ CWUtils/src/main/java/com/clashwars/cwutils/config/TipConfig.java
@@ -2,25 +2,28 @@ package com.clashwars.cwutils.config;
 
 import com.clashwars.cwutils.config.internal.EasyConfig;
 import com.clashwars.cwutils.util.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class TipConfig extends EasyConfig {
 
-    public String[] tips = new String[] {};
+    public List<String> tips = new ArrayList<String>();
 
     public TipConfig(String fileName) {
         this.setFile(fileName);
     }
 
     public String getTip(int ID) {
-        return tips[ID];
+        return tips.get(ID);
     }
 
     public String getRandomTip() {
-        return tips[Utils.random(0, tips.length - 1)];
+        return tips.get(Utils.random(0, tips.size() - 1));
     }
 
-    public String[] getTips() {
+    public List<String> getTips() {
         return tips;
     }
 }
